f17d23709889c997b4358debe2fd45eb9282afbb
akshata-patil24 <128469987+akshata-patil24@users.noreply.github.com>
2023-03-27 16:29:38 +0530

    Update streamlit_ap.py

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -44,7 +44,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur.header("What fruit would you like to add?")
 my_data_rows = my_cur.fetchall()
 streamlit.write("Add my fruit", fruit_choice)
