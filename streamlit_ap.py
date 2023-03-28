181d4ed2baf22c50aa036c7058857c3c607eb749
akshata-patil24 <128469987+akshata-patil24@users.noreply.github.com>
2023-03-28 11:03:27 +0530

    Update streamlit_ap.py

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -4,15 +4,15 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
  	 
- 	streamlit.title('My Moms New Healthy Diner')
+streamlit.title('My Moms New Healthy Diner')
  	 
- 	streamlit.header('Breakfast Favorites')
- 	streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
- 	streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
- 	streamlit.text('🐔 Hard-Boiled Free-Range Egg')
- 	streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
  	 
- 	streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
  	 
  	#import pandas
  	my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
